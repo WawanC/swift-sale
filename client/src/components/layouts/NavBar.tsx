09266882dbc034df01981752ae2c20b541670297
Kurniawan Cristianto <kurniawancristianto@gmail.com>
2023-09-23 22:05:53 +0700

    navbar mobile ui view

--- client/src/components/layouts/NavBar.tsx
+++ client/src/components/layouts/NavBar.tsx
@@ -4,29 +4,68 @@ import CartIcon from "../icons/CartIcon.tsx";
 import LogoutIcon from "../icons/LogoutIcon.tsx";
 import { useGetMe, useLogout } from "../../hooks/Auth.tsx";
 import { useGetCarts } from "../../hooks/Cart.tsx";
+import { useCallback, useState } from "react";
+import MenuIcon from "../icons/MenuIcon.tsx";
 
 const NavBar = () => {
   const getMe = useGetMe();
   const logout = useLogout();
   const getCarts = useGetCarts();
 
+  const [isSideMenuOpen, setIsSideMenuOpen] = useState(false);
+
+  const openSideMenu = useCallback(
+    () => setIsSideMenuOpen(true),
+    [setIsSideMenuOpen],
+  );
+
+  const closeSideMenu = useCallback(
+    () => setIsSideMenuOpen(false),
+    [setIsSideMenuOpen],
+  );
+
   return (
     <nav
-      className={`px-16 py-2 w-full bg-accent text-primary flex items-center`}
+      className={`px-16 py-2 bg-accent text-primary 
+      flex items-center justify-center relative`}
     >
+      {/* Side Menu Button Icon */}
+      <button className={`absolute left-4 md:hidden`} onClick={openSideMenu}>
+        <MenuIcon className={"w-8 aspect-square"} />
+      </button>
+
+      {/* App Logo */}
       <Link to={"/"} className={`text-2xl font-bold`}>
         SwiftSale
       </Link>
-      <div className={`flex-1 px-16 flex justify-center`}>
+
+      {/* Search Bar */}
+      <div className={`flex-1 px-16 hidden md:flex justify-center `}>
         <input
           type="text"
-          className={`w-1/2 rounded text-xl p-1 px-4 text-accent outline-none`}
+          className={`min-w-[50%] rounded text-xl p-1 px-4 text-accent outline-none`}
           placeholder={"Search Products..."}
         />
       </div>
-      <ul className={"text-xl flex gap-8 items-center"}>
+
+      {/* Side Menu Backdrop Element */}
+      {isSideMenuOpen && (
+        <div
+          className={`fixed md:hidden inset-0 bg-black opacity-50 z-10`}
+          onClick={closeSideMenu}
+        />
+      )}
+      {/* Actions Menu */}
+      <ul
+        className={`text-xl text-center 
+        md:flex md:static md:flex-row md:w-auto md:h-fit md:py-4
+        ${
+          isSideMenuOpen ? "flex" : "hidden"
+        } bg-accent left-0 top-0 bottom-0 z-20
+        fixed flex-col gap-8 w-[50%] px-8 py-32 h-screen`}
+      >
         {!getMe.data.userId ? (
-          <div className={`flex gap-8`}>
+          <>
             <Link
               to={"/login"}
               className={`text-secondary underline-offset-8 hover:text-primary hover:underline`}
@@ -39,13 +78,16 @@ const NavBar = () => {
             >
               Register
             </Link>
-          </div>
+          </>
         ) : (
           <>
-            <div className={`flex gap-4`}>
+            <div className={`flex flex-col md:flex-row gap-4`}>
               <Link to={"/account"}>
+                <span className={`md:hidden`}>My Account</span>
                 <AccountIcon
-                  className={"w-8 h-8 stroke-secondary hover:stroke-primary"}
+                  className={
+                    "w-8 h-8 stroke-secondary hover:stroke-primary hidden md:block"
+                  }
                 />
               </Link>
               <Link to={"/cart"} className={`relative`}>
@@ -58,15 +100,21 @@ const NavBar = () => {
                     {getCarts.totalCount}
                   </div>
                 )}
+                <span className={`md:hidden`}>My Cart</span>
                 <CartIcon
-                  className={"w-8 h-8 stroke-secondary hover:stroke-primary"}
+                  className={
+                    "w-8 h-8 stroke-secondary hover:stroke-primary hidden md:block"
+                  }
                 />
               </Link>
             </div>
-            <div className={"w-1 h-8 border-r-2 border-secondary"} />
+            <div
+              className={"w-1 h-8 border-r-2 border-secondary hidden md:block"}
+            />
             <button onClick={() => logout.mutate()}>
+              <span className={`md:hidden`}>Logout</span>
               <LogoutIcon
-                className={`w-8 h-8 stroke-secondary hover:stroke-primary`}
+                className={`w-8 h-8 stroke-secondary hover:stroke-primary hidden md:block`}
               />
             </button>
           </>
